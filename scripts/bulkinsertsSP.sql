a69019f4ae9f939d0db587275ed4ea7aa51cc3b5
CarlzMax <ksutherland@mail.regent.edu>
2025-12-13 09:37:06 -0500

    Update bulkinsertsSP.sql

--- scripts/bulkinsertsSP.sql
+++ scripts/bulkinsertsSP.sql
@@ -12,65 +12,88 @@ EXEC bronze.load_bronze
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	PRINT '==================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '==================================';
 
-TRUNCATE TABLE bronze.crm_cust_info;
+	PRINT '----------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '----------------------------------';
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT'>> Truncating Table: bronze.crm_cust_info'
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-TRUNCATE TABLE bronze.crm_prd_info;
+	PRINT'>> Inserting Data Into bronze.crm_cust_info'
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\crm_prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT'>> Truncating Table: bronze.crm_prd_info'
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT'>> Inserting Data Into bronze.crm_prd_info'
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\crm_prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT'>> Truncating Table: bronze.crm_sales_details'
+	TRUNCATE TABLE bronze.crm_sales_details;
 
-TRUNCATE TABLE bronze.erp_cust_az12;
+	PRINT'>> Inserting Data Into bronze.crm_sales_details'
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\cust_az12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '----------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '----------------------------------';
 
-TRUNCATE TABLE bronze.erp_loc_a101;
+	PRINT'>> Truncating Table: bronze.erp_cust_az12'
+	TRUNCATE TABLE bronze.erp_cust_az12;
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\loc_a101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT'>> Inserting Data Into bronze.erp_cust_az12'
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+	PRINT'>> Truncating Table: bronze.erp_loc_a101'
+	TRUNCATE TABLE bronze.erp_loc_a101;
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\px_cat_g1v2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT'>> Inserting Data Into bronze.erp_loc_a101'
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+
+	PRINT'>> Truncating Table: bronze.erp_px_cat_g1v2'
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	PRINT'>> Inserting Data Into bronze.erp_px_cat_g1v2'
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\Kai Sutherland\OneDrive\Desktop\Microsoft Learning Materials\MultipleTables\SQL Materials\sql-data-warehouse-project\datasets\source_crm\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 END
 
