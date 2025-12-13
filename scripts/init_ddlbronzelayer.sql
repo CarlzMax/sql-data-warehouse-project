1b9b235ba2066758e9b53ff7d570c2e07fa2987d
CarlzMax <ksutherland@mail.regent.edu>
2025-12-12 19:03:05 -0500

    Create init_ddlbronzelayer.sql

---
+++ scripts/init_ddlbronzelayer.sql
@@ -0,0 +1,65 @@
+/*
+
+==============================================================
+Create Bronze Layer DDL Tables
+==============================================================
+Script Purpose: 
+  This script creates all of the bronze layer DDL tables for the source systems CRM and ERP.
+  Additionally, checks for existing tables in SQL Server and replace them with the new ones
+  should the tables exist and need to be replaced. 
+
+*/
+
+
+IF OBJECT_ID ('bronze.crm_cust_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+IF OBJECT_ID ('bronze.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+IF OBJECT_ID ('bronze.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101
+CREATE TABLE bronze.erp_loc_a101 (
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+	);
+
+IF OBJECT_ID ('bronze.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
+);
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+);
+
+
+
