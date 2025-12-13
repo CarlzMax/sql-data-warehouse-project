786ad4c1c4f4502f8929795d8eddbb5f5ff9a0e4
CarlzMax <ksutherland@mail.regent.edu>
2025-12-13 11:21:01 -0500

    Update bulkinsertsSP.sql

--- scripts/bulkinsertsSP.sql
+++ scripts/bulkinsertsSP.sql
@@ -1,4 +1,24 @@
 /*
+
+==========================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+==========================================================
+Script Purpose: 
+	This stored procedure loads data into the 'bronze' schema from external CSV files.
+	It performs the following actions: 
+	- Truncates the bronze tables before loading data.
+	- Uses the 'BULK INSERT' command to load data from CSV files to bronze tables.
+
+Parameters:
+	None.
+This stored procedure does not accept any parameters or return any values.
+
+Usage Example: 
+	EXEC bronze.load_bronze;
+===========================================================
+
+*/
+/*
 ================================================
 This is a stored procedure for the BULK INSERTs
 ================================================
